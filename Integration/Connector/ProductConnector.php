5ce7435c3ce94ab0592a76223f275b4764425d4a
Andrew Danilchenko <adanilchenko@oroinc.com>
2019-03-13 10:02:42 +0200

    NEO-8: Fix product connector start and schema update button - Product connector must be skipped when user clicks Schedule Sync button for not updated scheme

--- Integration/Connector/ProductConnector.php
+++ Integration/Connector/ProductConnector.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\AkeneoBundle\Integration\Connector;
 
 use Oro\Bundle\AkeneoBundle\Integration\AkeneoTransport;
+use Oro\Bundle\AkeneoBundle\Placeholder\SchemaUpdateFilter;
 use Oro\Bundle\EntityBundle\Helper\FieldHelper;
 use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
@@ -30,6 +31,11 @@ class ProductConnector extends AbstractConnector implements ConnectorInterface, 
      * @var FieldHelper
      */
     protected $fieldHelper;
+
+    /**
+     * @var SchemaUpdateFilter
+     */
+    protected $schemaUpdateFilter;
 
     /**
      * {@inheritdoc}
@@ -92,7 +98,7 @@ class ProductConnector extends AbstractConnector implements ConnectorInterface, 
             }
         }
 
-        return $hasAkeneoFields;
+        return $hasAkeneoFields && !$this->needToUpdateSchema($integration);
     }
 
     /**
@@ -109,6 +115,14 @@ class ProductConnector extends AbstractConnector implements ConnectorInterface, 
     public function setFieldHelper(FieldHelper $fieldHelper): void
     {
         $this->fieldHelper = $fieldHelper;
+    }
+
+    /**
+     * @param SchemaUpdateFilter $schemaUpdateFilter
+     */
+    public function setSchemaUpdateFilter(SchemaUpdateFilter $schemaUpdateFilter): void
+    {
+        $this->schemaUpdateFilter = $schemaUpdateFilter;
     }
 
     /**
@@ -139,5 +153,16 @@ class ProductConnector extends AbstractConnector implements ConnectorInterface, 
         $iterator->append($this->transport->getProductModels(self::PAGE_SIZE));
 
         return $iterator;
+    }
+
+    /**
+     * Checks if schema is changed and need to update it.
+     *
+     * @param Channel $integration
+     * @return bool
+     */
+    private function needToUpdateSchema(Channel $integration): bool
+    {
+        return $this->schemaUpdateFilter->isApplicable($integration, Product::class);
     }
 }
