29579c3ccf6c62ffaa86561b5f19afea2262b53f
Serhii Zhuravel <szhuravel@oroinc.com>
2019-12-31 16:43:17 +0200

    BUGFIX: prevent the deletion of existing product images when new or updated images has been done on Akeneo Side. Fixes #25

--- ImportExport/Strategy/ProductImageImportStrategy.php
+++ ImportExport/Strategy/ProductImageImportStrategy.php
@@ -51,20 +51,6 @@ class ProductImageImportStrategy extends ConfigurableAddOrReplaceStrategy implem
     }
 
     /**
-     * @param ProductImage $entity
-     *
-     * @return object
-     */
-    protected function afterProcessEntity($entity)
-    {
-        if ($this->isImageDuplicate($entity)) {
-            return null;
-        }
-
-        return parent::afterProcessEntity($entity);
-    }
-
-    /**
      * Denormalizer sets wrong keys so ProductImage::removeType doesn't work.
      *
      * @param ProductImage $entity
@@ -76,28 +62,5 @@ class ProductImageImportStrategy extends ConfigurableAddOrReplaceStrategy implem
                 unset($entity->getTypes()[$key]);
             }
         }
-    }
-
-    /**
-     * @param ProductImage $entity
-     *
-     * @return bool
-     *
-     * @throws \Doctrine\ORM\NoResultException
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    private function isImageDuplicate(ProductImage $entity): bool
-    {
-        $count = $this->doctrineHelper->getEntityRepository($entity)->createQueryBuilder('pi')
-            ->select('count(pi.id)')
-            ->leftJoin('pi.image', 'i')
-            ->where('pi.product = :product')
-            ->andWhere('i.originalFilename = :name')
-            ->setParameter('product', $entity->getProduct())
-            ->setParameter('name', $entity->getImage()->getOriginalFilename())
-            ->getQuery()
-            ->getSingleScalarResult();
-
-        return (int)$count > 0;
     }
 }
