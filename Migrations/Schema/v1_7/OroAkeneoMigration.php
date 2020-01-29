657835680ad0816e399619a13696fb9771fe116a
Ievgenii Myshusta <imyshusta@magecore.com>
2020-01-29 16:44:50 +0200

    GRKAS1-29: Add configuration to customize ProductUnitPrecision for Akeneo connector - Define an attribute name from Akeneo in configuration, use it’s value to set unit and precision during imports

---
+++ Migrations/Schema/v1_7/OroAkeneoMigration.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Oro\Bundle\AkeneoBundle\Migrations\Schema\v1_7;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+/**
+ * @SuppressWarnings(PHPMD.TooManyMethods)
+ * @SuppressWarnings(PHPMD.ExcessiveClassLength)
+ */
+class OroAkeneoMigration implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getMigrationVersion()
+    {
+        return 'v1_7';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        /* Tables generation */
+        $this->updateOroIntegrationTransportTable($schema);
+    }
+
+    /**
+     * Create oro_integration_transport table
+     */
+    protected function updateOroIntegrationTransportTable(Schema $schema)
+    {
+        $table = $schema->getTable('oro_integration_transport');
+        $table->addColumn('akeneo_product_unit_attribute', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('akeneo_unit_precision_attr', 'string', ['notnull' => false, 'length' => 255]);
+    }
+}
