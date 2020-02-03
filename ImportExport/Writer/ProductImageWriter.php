be7a96215fa177180b50ba9a370725453609321a
julien <julien@kiboko.fr>
2020-02-03 11:56:12 +0100

    Revert inception ... Product images imports are now fully working

--- ImportExport/Writer/ProductImageWriter.php
+++ ImportExport/Writer/ProductImageWriter.php
@@ -41,6 +41,18 @@ class ProductImageWriter extends PersistentBatchWriter
 
         $this->products = null;
         $this->images = null;
+
+        $this
+            ->getImportContext()
+            ->setValue(ProductImageImportStrategy::DUPLICATED_IMAGES, null);
+    }
+
+    /**
+     * @return ContextInterface
+     */
+    private function getImportContext()
+    {
+        return $this->contextRegistry->getByStepExecution($this->stepExecution);
     }
 
     protected function saveItems(array $items, EntityManager $em)
@@ -51,6 +63,30 @@ class ProductImageWriter extends PersistentBatchWriter
         foreach ($items as &$item) {
             $this->products[$item->getProduct()->getId()] = $item->getProduct()->getId();
             $this->images[] = $item->getImage()->getId();
+
+            $this->addDuplicates($item);
+
         }
+    }
+
+    /**
+     * @param ProductImage $item
+     *
+     * @return void
+     */
+    private function addDuplicates(ProductImage $item): void
+    {
+        $duplicateImages = (array)$this
+            ->getImportContext()
+            ->getValue(ProductImageImportStrategy::DUPLICATED_IMAGES);
+
+        if (isset($duplicateImages[$item->getProduct()->getId()])) {
+            /** @var \Oro\Bundle\AttachmentBundle\Entity\File $image */
+            foreach ($duplicateImages[$item->getProduct()->getId()] as $image) {
+                $this->images[] = $image;
+            }
+        }
+
+        $this->images = array_unique($this->images);
     }
 }
