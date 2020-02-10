ffa3f6501adb3a69342e071ffbc2a32af92c5a16
Sylvain Rayé <sly@diglin.com>
2020-02-10 12:10:54 +0100

    Revert duplicate image logic as it's still not working

--- ImportExport/Strategy/ProductImageImportStrategy.php
+++ ImportExport/Strategy/ProductImageImportStrategy.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\AkeneoBundle\ImportExport\Strategy;
 
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Oro\Bundle\BatchBundle\Item\Support\ClosableInterface;
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrategy;
 use Oro\Bundle\ProductBundle\Entity\ProductImage;
@@ -53,7 +55,23 @@ class ProductImageImportStrategy extends ConfigurableAddOrReplaceStrategy implem
     }
 
     /**
+     * @param ProductImage $entity
+     *
+     * @return object|null
+     */
+    protected function afterProcessEntity($entity)
+    {
+        if ($this->isImageDuplicate($entity)) {
+            return null;
+        }
+
+        return parent::afterProcessEntity($entity);
+    }
+
+    /**
      * Denormalizer sets wrong keys so ProductImage::removeType doesn't work.
+     *
+     * @param ProductImage $entity
      */
     private function removeImageTypes(ProductImage $entity)
     {
@@ -62,5 +80,46 @@ class ProductImageImportStrategy extends ConfigurableAddOrReplaceStrategy implem
                 unset($entity->getTypes()[$key]);
             }
         }
+    }
+
+    /**
+     * @param ProductImage $entity
+     *
+     * @return bool
+     */
+    private function isImageDuplicate(ProductImage $entity): bool
+    {
+        try {
+            $image = $this->doctrineHelper->getEntityRepository($entity)->createQueryBuilder('pi')
+                ->select('pi')
+                ->leftJoin('pi.image', 'i')
+                ->where('pi.product = :product')
+                ->andWhere('i.originalFilename = :name')
+                ->setParameter('product', $entity->getProduct())
+                ->setParameter('name', $entity->getImage()->getOriginalFilename())
+                ->getQuery()
+                ->getSingleResult();
+        } catch (NoResultException $e) {
+            return false;
+        } catch (NonUniqueResultException $e) {
+            return true;
+        }
+
+        $this->addDuplicatedImages($image);
+        return true;
+    }
+
+    /**
+     * @param \Oro\Bundle\ProductBundle\Entity\ProductImage $image
+     *
+     * @return void
+     */
+    private function addDuplicatedImages(ProductImage $image): void
+    {
+        $duplicatedImages = $this->context->getValue(self::DUPLICATED_IMAGES);
+        $duplicatedImages = $duplicatedImages ?? [];
+        $duplicatedImages[$image->getProduct()->getId()][] = $image->getImage()->getId();
+
+        $this->context->setValue(self::DUPLICATED_IMAGES, $duplicatedImages);
     }
 }
