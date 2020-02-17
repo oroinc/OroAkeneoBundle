ce8f2bd963263a0626af574dd6b2758ca37b4162
julien <julien@kiboko.fr>
2020-02-17 16:58:57 +0100

    Update migration according to migration already passed on production server

---
+++ Migrations/Schema/v1_8_1/OroAkeneoMigration.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Oro\Bundle\AkeneoBundle\Migrations\Schema\v1_8_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+/**
+ * @SuppressWarnings(PHPMD.TooManyMethods)
+ * @SuppressWarnings(PHPMD.ExcessiveClassLength)
+ */
+class OroAkeneoMigration implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getMigrationVersion()
+    {
+        return 'v1_7';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        /** Tables generation **/
+        $this->updateOroIntegrationTransportTable($schema);
+    }
+
+    /**
+     * Create oro_integration_transport table
+     */
+    protected function updateOroIntegrationTransportTable(Schema $schema)
+    {
+        $table = $schema->getTable('oro_integration_transport');
+
+        if (!$table->hasColumn('akeneo_product_unit_attribute')) {
+            $table->addColumn('akeneo_product_unit_attribute', 'string', ['notnull' => false, 'length' => 255]);
+        }
+
+        if (!$table->hasColumn('akeneo_unit_precision_attr')) {
+            $table->addColumn('akeneo_unit_precision_attr', 'string', ['notnull' => false, 'length' => 255]);
+        }
+    }
+}
