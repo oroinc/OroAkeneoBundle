e778c783282d6200482670140a5acd7ef036a346
Sylvain Rayé <sly@diglin.com>
2020-05-06 15:52:20 +0200

    BUGFIX: re-introduce the method `setProductUnitDiscovery` deleted by a previous merge

--- ImportExport/DataConverter/ProductDataConverter.php
+++ ImportExport/DataConverter/ProductDataConverter.php
@@ -5,6 +5,7 @@ namespace Oro\Bundle\AkeneoBundle\ImportExport\DataConverter;
 use Doctrine\Common\Util\Inflector;
 use Oro\Bundle\AkeneoBundle\Entity\AkeneoSettings;
 use Oro\Bundle\AkeneoBundle\ImportExport\AkeneoIntegrationTrait;
+use Oro\Bundle\AkeneoBundle\ProductUnit\ProductUnitDiscoveryInterface;
 use Oro\Bundle\AkeneoBundle\Tools\AttributeFamilyCodeGenerator;
 use Oro\Bundle\AkeneoBundle\Tools\AttributeTypeConverter;
 use Oro\Bundle\AkeneoBundle\Tools\Generator;
@@ -69,6 +70,11 @@ class ProductDataConverter extends BaseProductDataConverter implements ContextAw
     public function setDoctrineHelper(DoctrineHelper $doctrineHelper)
     {
         $this->doctrineHelper = $doctrineHelper;
+    }
+
+    public function setProductUnitDiscovery(ProductUnitDiscoveryInterface $productUnitDiscovery): void
+    {
+        $this->productUnitDiscovery = $productUnitDiscovery;
     }
 
     /**
