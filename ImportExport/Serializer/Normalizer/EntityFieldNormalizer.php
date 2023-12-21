4a9ca969759e524434f549d382b933ed42dfd58e
Gregori De Vuyst <gregori.devuyst@phpro.be>
2023-12-21 15:07:48 +0100

    add priority enum config

--- ImportExport/Serializer/Normalizer/EntityFieldNormalizer.php
+++ ImportExport/Serializer/Normalizer/EntityFieldNormalizer.php
@@ -33,6 +33,9 @@ class EntityFieldNormalizer extends BaseEntityFieldNormalizer
             'is_default' => [
                 self::CONFIG_TYPE => self::TYPE_BOOLEAN,
             ],
+            'priority' => [
+                self::CONFIG_TYPE => self::TYPE_INTEGER,
+            ],
         ];
     }
 }
